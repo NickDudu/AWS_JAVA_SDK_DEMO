9b18bb33dd4b3733b51c34ab382eede8a2612378
Hu <huxiaod@08f8bc6ba193.ant.amazon.com>
2022-11-08 10:19:34 +0800

    second commit to copy object in the loop

--- src/main/java/com/nick/sdk/s3/GetTheLatestFileByDay.java
+++ src/main/java/com/nick/sdk/s3/GetTheLatestFileByDay.java
@@ -20,6 +20,12 @@ import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.Date;
 import java.util.List;
+import software.amazon.awssdk.services.s3.model.CopyObjectRequest;
+import software.amazon.awssdk.services.s3.model.CopyObjectResponse;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class GetTheLatestFileByDay {
     public static void main(String[] args) {
@@ -54,6 +60,11 @@ public class GetTheLatestFileByDay {
             if (myValue.lastModified().isAfter(instant) == true) {
                 System.out.print("\n" + myValue.key());
                 //System.out.print("\n" + instant);
+                String objectKey = myValue.key();
+                String fromBucket = "testnickhu";
+                String toBucket = "nickbilling";
+                System.out.format("Copying object %s from bucket %s to %s\n", objectKey, fromBucket, toBucket);
+                copyBucketObject (s3, fromBucket, objectKey, toBucket);
             } else {
                 //not right
             }
@@ -61,6 +72,34 @@ public class GetTheLatestFileByDay {
         }
 
         s3.close();
+    }
+
+    public static String copyBucketObject (S3Client s3, String fromBucket, String objectKey, String toBucket) {
+
+        String encodedUrl = "";
+        try {
+            encodedUrl = URLEncoder.encode(fromBucket + "/" + objectKey, StandardCharsets.UTF_8.toString());
+
+        } catch (UnsupportedEncodingException e) {
+            System.out.println("URL could not be encoded: " + e.getMessage());
+        }
+
+        CopyObjectRequest copyReq = CopyObjectRequest.builder()
+                .sourceBucket(fromBucket)
+                .sourceKey(objectKey)
+                .destinationBucket(toBucket)
+                .destinationKey(objectKey)
+                .build();
+
+        try {
+            CopyObjectResponse copyRes = s3.copyObject(copyReq);
+            return copyRes.copyObjectResult().toString();
+
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+        return "";
     }
 
     public static void createBucket( S3Client s3Client, String bucketName) {
